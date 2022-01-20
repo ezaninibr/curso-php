3852279b185e76337a52c9980dbd9c7fdfce2218
Zanini <everton.zanini@yahoo.com>
2022-01-20 12:05:00 -0300

    Aula de Tipos/Inteiros concluída

--- tipos/int.php
+++ tipos/int.php
@@ -1,1 +1,15 @@
 <div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo "<br>";
+
+var_dump(11);
+echo "<br>";
+
+echo PHP_INT_MIN, "<br>";
+echo PHP_INT_MAX, "<br>";
+
+echo 011, "<br>"; // Base Octal;
+echo 0b11, "<br>"; // Base Binária
+echo 0xb11, "<br>"; // Base Hexadecimal
