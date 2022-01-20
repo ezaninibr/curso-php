5dea2d7069409add482d8d6754f3875e8892f9fd
Zanini <everton.zanini@yahoo.com>
2022-01-20 14:40:37 -0300

    Desafio de Precedência concluído

--- index.php
+++ index.php
@@ -43,6 +43,9 @@
                     <ul>
                         <li><a href="exercicio.php?dir=tipos&file=aritimeticas">Aritiméticas</a></li>
                     </ul>
+                    <ul>
+                        <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedência</a></li>
+                    </ul>
                 </div>
             </nav>
         </div>
